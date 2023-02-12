6bc08ba3b539e4bff16b6137be7da1cb7c568e9c
Paolo Saturnino <paolosaturnino@gmail.com>
2023-02-12 17:39:50 +0800

    Act3

--- app/src/main/java/com/example/sharedpreferenceapp/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferenceapp/MainActivity.java
@@ -9,43 +9,56 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nameField, emailField;
-    private Button saveButton, retrieveButton, clearButton;
+    private EditText agentName, realEstatePriAmtPerUnit, numberRealEstSold, agentComm;
+    private TextView agentNameResult, commissionNetPay;
+    private Button submitButton, clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameField = findViewById(R.id.name_field);
-        emailField = findViewById(R.id.email_field);
-        saveButton = findViewById(R.id.save_button);
-        retrieveButton = findViewById(R.id.retrieve_button);
+        agentName = findViewById(R.id.agent_name);
+        realEstatePriAmtPerUnit = findViewById(R.id.real_estate_per_unit);
+        numberRealEstSold = findViewById(R.id.num_of_real_est_sold);
+        agentComm = findViewById(R.id.agents_comm);
+        submitButton = findViewById(R.id.submit_button);
         clearButton = findViewById(R.id.clear_button);
+        agentNameResult = findViewById(R.id.agent_name_result);
+        commissionNetPay = findViewById(R.id.comission_net_pay);
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveData("email_data", emailField.getText().toString());
-                saveData("name_data", nameField.getText().toString());
-            }
-        });
+                saveData("agent_name", agentName.getText().toString());
+                saveData("real_estate", realEstatePriAmtPerUnit.getText().toString());
+                saveData("real_estate_sold", numberRealEstSold.getText().toString());
+                saveData("agent_comm", agentComm.getText().toString());
+                String agent_name = loadData("agent_name");
+                String real_estate = loadData("real_estate");
+                String real_estate_sold = loadData("real_estate_sold");
+                String agent_comm = loadData("agent_comm");
 
-        retrieveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String nameData = loadData("name_data");
-                String emailData = loadData("email_data");
-                emailField.setText(emailData);
-                nameField.setText(nameData);
+                agentNameResult.setText(agent_name);
+                double priAmt = Double.valueOf(real_estate);
+                int numSold = Integer.valueOf(real_estate_sold);
+                double VAT = (priAmt * numSold) * 0.10; // constant 10%
+                double aComm = Double.valueOf(agent_comm);
+                double comNetPay = ((priAmt * numSold) * (aComm * 0.01)) - VAT;
+                commissionNetPay.setText(Double.toString(comNetPay));
             }
         });
 
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                emailField.setText("");
-                nameField.setText("");
+                agentName.setText("");
+                realEstatePriAmtPerUnit.setText("");
+                numberRealEstSold.setText("");
+                agentComm.setText("");
+                agentNameResult.setText("");
+                commissionNetPay.setText("");
             }
         });
     }
